b127a39de20c043b4e7468a37129b6d45d239d7e
abraham <abrahamosazee3@gmail.com>
2023-09-24 20:37:54 +0100

    fix error

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -2,10 +2,9 @@
 """
 2d matrix rotate interview task
 """
-from typing import List
 
 
-def rotate_2d_matrix(matrix: List) -> None:
+def rotate_2d_matrix(matrix: list) -> None:
     matrix_len = len(matrix)
 
     # first transpose the matrix
